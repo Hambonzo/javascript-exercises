2a2315aca5b2e438637fe4237a27c6546c47df4e
Hambonzo <ledham@outlook.com>
2025-09-11 16:01:18 +0100

    created a function that removed designated value

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, number) {
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] === number) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
