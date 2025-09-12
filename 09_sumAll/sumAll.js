7de024648922cb668e216c7ddfb199f4eda015fd
Hambonzo <ledham@outlook.com>
2025-09-12 14:12:33 +0100

    sumAll function that accepts positive integers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (start < 0 || end < 0){ 
+        return "ERROR";
+    }
 
-};
+    if(typeof start !== "number" || typeof end !== "number" ||
+        !Number.isInteger(start) || !Number.isInteger(end)
+    ){  
+        return "ERROR";
+    }
+
+    let total = 0;
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            total += i;
+        }
+
+        return total;
+    } else if (start > end) {
+        for (let i = start; i >= end; i--) {
+            total += i;
+        }
+
+        return total;
+    }
+}
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
