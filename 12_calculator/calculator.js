417067b3bf421fd80f90f12166d9077433d2d2aa
Hambonzo <ledham@outlook.com>
2025-09-22 15:24:05 +0100

    used reduce, loops and basic arithmetic

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, number) => acc + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, number) => acc * number, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let total = 1;
+  for (let i = 0; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let factorial = 1;
+  for (let i = a; a >= 1; i--) {
+    factorial *= i;
+  }
+  return factorial
 };
 
 // Do not edit below this line
