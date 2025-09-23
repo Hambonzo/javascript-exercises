6394a98366ad478610923b7cb911b1dcb915f81e
Hambonzo <ledham@outlook.com>
2025-09-23 15:01:12 +0100

    created a fibonacci sequence by generating values
    then using a number - 1 to index

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0) {
+        return("OOPS");
+    }
+    if(typeof number === "string"){
+        number = +number;
+    }
 
+    let arr = [1,1,2,3,5,8];
+    for(let i = 6; i <= 25; i++) {
+        let newVal = (arr[arr.length - 1]) + (arr[arr.length - 2]);
+        arr.push(newVal);
+    }
+    return arr[number - 1];
 };
 
 // Do not edit below this line
