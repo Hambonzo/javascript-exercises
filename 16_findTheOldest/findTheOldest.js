50a9f4ad298fb5e886a5ebd10947a3f624c59953
Hambonzo <ledham@outlook.com>
2025-09-24 13:49:35 +0100

    created a find oldest object function
    used date.getfullyear in cases person didn't have year of death

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    const currentYear = new Date().getFullYear()
 
+    let oldest = arr.reduce((acc, person) => {
+        let death;
+        if (person.yearOfDeath === undefined) {
+            death = currentYear;
+        }
+        else {
+            death = person.yearOfDeath; 
+        }
+        const age = death - person.yearOfBirth;
+
+        let accDeath;
+        if (acc.yearOfDeath === undefined) {
+            accDeath = currentYear;
+        } else {
+            accDeath = acc.yearOfDeath;
+        }
+
+        const accAge = accDeath - acc.yearOfBirth;
+
+        if (age > accAge) {
+            return person;
+        } else {
+            return acc;
+        }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
